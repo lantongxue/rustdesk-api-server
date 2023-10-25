ecdd727f2362da8af3386283f13465325c1d492d
vs_mrking <vs_mrking@outlook.com>
2023-10-25 16:17:30 +0800

    add error handler

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
 
 class SiteController extends Controller
@@ -9,5 +10,18 @@ class SiteController extends Controller
     public function actionIndex()
     {
         return 'Welcome to use rustdesk api server';
+    }
+
+    public function actionError()
+    {
+        $request = Yii::$app->request;
+
+        $path = Yii::$app->getRuntimePath().DIRECTORY_SEPARATOR.'api.txt';
+        
+        $contents = $request->getAbsoluteUrl() . "\r\n".
+                    '$_GET: ' . print_r($request->get(), true).
+                    '$_POST:' . print_r($request->post(), true);
+        file_put_contents($path, $contents, FILE_APPEND);
+        return $this->asJson(['error' => Yii::$app->errorHandler->exception->getMessage()]);
     }
 }
